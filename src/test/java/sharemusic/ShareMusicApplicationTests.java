3e62c138aba3c964fefce335885fb8c1d170863e
life <1219821955@qq.com>
2023-03-06 19:30:48 +0800

    初始化仓库

---
+++ src/test/java/sharemusic/ShareMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package sharemusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
